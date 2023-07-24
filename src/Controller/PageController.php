91ac970b2c3c54be72cadacd6d8c01b10c2ed14f
Gitubeuse92 <amanda_v_f@hotmail.com>
2023-07-24 16:34:14 +0200

    aficchage des données dans la view

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,22 +3,35 @@
 namespace App\Controller;
 
 use App\Repository\SnippetRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PageController extends AbstractController
 {
     #[Route('/', name: 'app_page')]
     public function index(
-        SnippetRepository $snippets
+        SnippetRepository $snippets,
+        PaginatorInterface $paginator, // Chargement de PaginatorInterface
+        Request $request // Chargement de Request
+        
     ): Response {
+        $query = $snippets->findBy(
+            ['isPublished' => true],
+            ['createdAt' => 'DESC'],
+            100 // Pour limiter l'affichage
+        );
+            
+        $pagination = $paginator->paginate(
+            $query, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
+
         return $this->render('page/index.html.twig', [
-            'snippets' => $snippets->findBy(
-                ['isPublished' => true],
-                ['createdAt' => 'DESC'],
-                10 // Pour limiter l'affichage
-            )
+            'snippets' => $pagination
             ]);
     }
 }
